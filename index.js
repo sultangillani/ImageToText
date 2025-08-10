792901ad4a2a8869a880013368237ec3e0cf721f
Sultangillani <sultangilani13@gmail.com>
2025-08-10 23:11:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -29,32 +29,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/', imageToText);
 
 
-/*const URL = require('./models/url');
-
-const dbURI = 'mongodb://127.0.0.1:27017/short-url'; 
-
-connectMongoDb(dbURI).then(() => console.log('MongoDB connected successfully!')).catch(err => console.error('MongoDB connection error:', err));
-
-app.set('view engine', 'ejs');
-app.set('views', path.resolve('./views'));
-
-app.use( express.json() );
-app.use( express.urlencoded({extended: false}) );
-
-app.use('/url', urlRoute);
-app.use('/', staticRoute);
-
-app.get('/url/:shortId', async (req, res) => {
-	const shortId = req.params.shortId;
-	const entry = await URL.findOneAndUpdate({ shortId }, {
-		$push: {
-			visitedHistory: {timestamp: Date.now() }
-		}
-	});
-	
-	res.redirect(entry.redirectURL);
-});*/
-
 app.listen(PORT,() => {
 	console.log('Server Started');
 });
